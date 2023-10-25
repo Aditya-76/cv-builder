917a9926db742e7b3a646a726d4c5666ad3bf30a
Aditya Shekhar <aditya7682@gmail.com>
2023-10-25 21:14:19 +0530

    Add left panel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,43 @@
 import { useState } from 'react'
+import Container from 'react-bootstrap/Container'
+import '../styles/App.css'
+import Banner from './Banner.jsx'
 
 function App() {
+  const personalDetails = {
+    name: 'Aditya Shekhar',
+    email: 'aditya7682@gmail.com',
+    phone: '8827487684',
+    address: 'Noida, India'
+  }
+  const education = [
+    {
+      school: 'Indian Institue of Technology, Gandhinagar',
+      degree: 'B.Tech',
+      grade: '6.58',
+      start: '2019',
+      end: '2023',
+    }
+  ]
+  const experience = [
+    {
+      company: 'The Odin Project',
+      position: 'Frontend Developer',
+      start: '2022',
+      end: 'current',
+      location: 'Bengaluru',
+      description: 'Built interctive web-apps using frontend technologies.'
+    }
+  ]
 
   return (
-    <>
-
-    </>
+    <div className="mainContainer">
+      <div className="leftContainer">
+        <Banner/>
+      </div>
+      <div className="centerContainer"></div>
+      <div className="rightContainer"></div>
+    </div>
   )
 }
 
---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import '../styles/Banner.css';
+
+function Banner() {
+    return (
+        <div className='banner'>
+            <h3>CV Generator</h3>
+            <footer>&copy; <a href="https://github.com/Aditya-76" target="_blank">Aditya-76</a></footer>
+        </div>
+    )
+}
+
+export default Banner;
